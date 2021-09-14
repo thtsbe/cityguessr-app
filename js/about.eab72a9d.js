ceb353e1c45d64d1a54b7232b2470e919a94213b
thtsbe <90325348+thtsbe@users.noreply.github.com>
2021-09-14 12:04:13 +0000

    Deploying to gh-pages from @ thtsbe/cityguessr-app@2d62438efc47f1553ba160fe31c866ea1429fe7c 🚀

---
+++ js/about.eab72a9d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,n,a){"use strict";a.r(n);var e=a("7a23"),c={class:"about"},o=Object(e["h"])("h1",null,"This is an about page",-1),s=[o];function u(t,n){return Object(e["t"])(),Object(e["g"])("div",c,s)}const b={};b.render=u;n["default"]=b}}]);
+//# sourceMappingURL=about.eab72a9d.js.map
