76b38fb391896e5bd1fd4863378f45d7303263fb
Spring, Timo <timo.spring@zuehlke.com>
2021-09-15 09:56:58 +0200

    Add rotating earth background

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="background"></div>
   <router-view />
 </template>
 
@@ -13,5 +14,29 @@
 
 body {
   margin: 0;
+  background-color: black;
+}
+
+.background {
+  animation: rotation 12s infinite linear;
+  background-image: url(/img/earth.04512e70.png);
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-color: black;
+  position: absolute;
+  left: -20%;
+  right: -20%;
+  height: 100vh;
+  background-position: bottom;
+  z-index: -1;
+
+  @keyframes rotation {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(359deg);
+    }
+  }
 }
 </style>
