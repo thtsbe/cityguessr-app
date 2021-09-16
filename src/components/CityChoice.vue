2a10484bb9e83f59c1f8d922bb091b0b51d7e3ca
thtsbe <thomas.tschachtli@zuehlke.com>
2021-09-16 10:09:33 +0200

    Revert "country names"
    This reverts commit 42fd497fc59d57150fa8311471e29600c630b547.

--- src/components/CityChoice.vue
+++ src/components/CityChoice.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button @click="click" :disabled="disabled"><b>{{ city.city }}</b> {{ city.country }}</button>
+    <button @click="click" :disabled="disabled">{{ option }}</button>
   </div>
 </template>
 
@@ -13,7 +13,7 @@ export default defineComponent({
   emits: ["selectCity"],
 
   props: {
-    city: {}
+    option: { type: String, required: true },
   },
 
   data() {
